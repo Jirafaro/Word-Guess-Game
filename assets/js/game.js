121acdbcf7794c0bd347cdd36a78d39b76e46594
Austin Robbins <jirafaro@gmail.com>
2019-06-12 17:57:28 -0700

    Fixed commas in array by using .join instead of .tostring. Fixed duplicate keyword error where only first keyword in array was pulled.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,5 @@
 // Create an array of words
-const word = ['injury','pip','underinsured','uninsured','collision','umbrella','one step at a time'];
+const word = ['injury','pip','underinsured','uninsured','collision','umbrella'];
 // Choose words randomly 
 let randNum = Math.floor(Math.random() * word.length);
 let choosenWord = word[randNum];
@@ -9,7 +9,6 @@ let underScore = [];
 
 // Dom Manipulation
 let docUnderScore = document.getElementsByClassName('underscores')
-let docrightWord = document.getElementsByClassName('rightguess')
 // docUnderScore[0].innerHTML = 'Hello';
 console.log(docUnderScore);
 
@@ -20,26 +19,44 @@ let generateunderScore = () => {
     }
     return underScore;
 }
+
 // testing
 console.log(generateunderScore());
 // Get users guess
 document.addEventListener('keypress', (event) => {
     let keyword = String.fromCharCode(event.keyCode);
 // if users guess is right 
-    if(choosenWord.indexOf(keyword) > -1) {
-    // add to right words array
-    rightWord.push(keyword);
-    //replace underscore with right letter
-    underScore[choosenWord.indexOf(keyword)] = keyword;
-    docUnderScore[0].innerHTML = underScore.toString(' ');
-    // check to see if user word matches guesses 
-    if(underScore.join('') == choosenWord) {
-        alert('You Win!')
-    }
-        } else 
-         wrongWord.push(keyword);
-    } )  
-// Generate underscores 
+for(let i=0; i < choosenWord.length; i++) {
+    if(choosenWord[i] === keyword) {
+        // add to right words array
+        rightWord.push(keyword);
+        //replace underscore with right letter
+        underScore[i] = keyword;
+        docUnderScore[0].innerHTML = underScore.join(' ');
+        // check to see if user word matches guesses 
+        if(underScore.join('') === choosenWord) {
+            alert('You Win!')
+        }
+            } else 
+             wrongWord.push(keyword);
+        }  
+})
+
+
+    // if(choosenWord.indexOf(keyword) > -1) {
+    // // add to right words array
+    // rightWord.push(keyword);
+    // //replace underscore with right letter
+    // underScore[choosenWord.indexOf(keyword)] = keyword;
+    // docUnderScore[0].innerHTML = underScore.join(' ');
+    // // check to see if user word matches guesses 
+    // if(underScore.join('') === choosenWord) {
+    //     alert('You Win!')
+    // }
+    //     } else 
+    //      wrongWord.push(keyword);
+    // }  
+    
 // Check if guess is right
 // if right push to right array 
 // if wrong push to wrong array
